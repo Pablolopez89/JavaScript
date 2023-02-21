92327144cfc0f825d47dba4ec92bf9a00a5080a1
Pablo Lopez <pablo.crzz@hotmail.com>
2023-02-21 18:13:54 -0300

    se agrego texto al html

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
 
-// Prompt Elija entre las opciones, ¿que necesita asegurar?
-
-// Opcion 1 - Auto
-// Opcion 2 - Moto
-// Opcion 3 - Bicicleta
-// Opcion 4 - Vida
-// Opcion 5 - Otros
-
-// let productoAsegurado = parseInt(prompt('Hola ¿Que necesita cotizar? \n1- Auto \n2- Moto \n3- Bicicleta'));
-
-// if (productoAsegurado = '1' || '2') {
-//     let modelo = prompt('Indique el modelo');
-
-// } else if (productoAsegurado = '3') {
-//     let precioBicicleta = prompt('Indique valor de la Bicicleta');
-
-// }
 let precio = [];
 // Pide al usuario que ingrese los datos del auto
 let numAutos = parseInt(prompt("Ingrese el número de autos a asegurar:"));
