fde4355c7dc7aad25ec30066a2382f968abb3c1f
Pablo Lopez <pablo.crzz@hotmail.com>
2023-02-21 21:32:28 -0300

    Se mejoro la ultima linea else if

--- js/script.js
+++ js/script.js
@@ -4,22 +4,24 @@ let precio = [];
 let numAutos = parseInt(prompt("Ingrese el número de autos a asegurar:"));
 
 for (let i = 0; i < numAutos; i++) {
-    let marca = prompt(`Ingrese la marca del auto: ${i+1}`);
-    let modelo = prompt(`Ingrese el modelo del auto: ${i+1}`);
-    let año = prompt(`Ingrese el año del auto: ${i+1}`);
+    let marca = prompt(`Ingrese la marca del auto: ${i + 1}`);
+    let modelo = prompt(`Ingrese el modelo del auto: ${i + 1}`);
+    let año = prompt(`Ingrese el año del auto: ${i + 1}`);
 
-let precioBase = 0; 
+    let precioBase = 0;
 
-if (año >= 2022) {
-    precioBase = 8000;
-} else if (año >= 2015 && año < 2022) {
-    precioBase = 5000;
-} else if (año >= 2010 && año < 2015) {
-    precioBase = 3000;
-} else if (año < 2010) {
-    alert('No tiene cotizacion por la antiguedad de su vehiculo');
-}
-precio.push (`La cotización para asegurar un ${marca} ${modelo} del año ${año} es de $${precioBase}.`);
+    if (año >= 2022) {
+        precioBase = 8000;
+    } else if (año >= 2015 && año < 2022) {
+        precioBase = 5000;
+    } else if (año >= 2010 && año < 2015) {
+        precioBase = 3000;
+    } else if (año < 2010) {
+        precioBase = "0 - No tiene cotizacion para su vehiculo"
+        alert('No tiene cotizacion por la antiguedad de su vehiculo');
+    }
+    precio.push(`La cotización para asegurar un ${marca} ${modelo} del año ${año} es de $${precioBase}.`);
+
 }
 
 // Muestra las tarifas de seguro de cada auto en una ventana de alerta
